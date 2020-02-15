38dbaa66a48208bc98830aff915cba940b373662
Lee Morin <Lealand.Morin@ucf.edu>
2020-02-15 16:18:48 -0500

    Created first function

---
+++ R/aggregress.R
@@ -0,0 +1,23 @@
+
+adj_wtd_lm_summary <- function(wtd_lm) {
+
+  # Copy the lm object.
+  adj_wtd_lm <- wtd_lm
+
+  # Replace the df for the residual (n - k).
+  adj_wtd_lm$df.residual <- sum(adj_wtd_lm$weights) - length(coef(adj_wtd_lm))
+
+  # Make a copy of the summary to adjust the R-bar_squared.
+  sum_adj_wtd_lm <- summary(adj_wtd_lm)
+
+  # Replace the value of the R-bar-squared.
+  sum_adj_wtd_lm$adj.r.squared <-
+    1 - (1 - sum_adj_wtd_lm$r.squared) *
+    (sum(adj_wtd_lm$weights) - 1) /
+    (sum(adj_wtd_lm$weights) - length(coef(adj_wtd_lm)))
+
+  # Return the summary with the adjusted weighted regression
+  # to replace the original individual-level data.
+  return(sum_adj_wtd_lm)
+
+}
