fcea1bd85f9697697d3ad72c5c84492213da3c56
Lee Morin <Lealand.Morin@ucf.edu>
2020-06-19 17:05:52 -0400

    Corrected and verified White HCCME on unaggregated data

--- R/agregress_het.R
+++ R/agregress_het.R
@@ -2,21 +2,26 @@
 
 #' Calculate the Heteroskedasticity-Consistent Covariance Matrix Estimator.
 #'
-#' \code{white_hccme} is a helper function to calculate the HCCME
+#' \code{white_hccme} is a helper function to calculate the HCCME,
+#' to test the calculation for un-aggregated data, i.e. data
+#' with weights either \code{NULL} or equal to 1.
 #'
-#' @param lm_summ is an object of class "\code{lm}" that is the output of a
+#' @param lm_in is an object of class "\code{lm}" that is the output of a
 #' linear regression model.
 #'
 #' @return A data frame of the form of \code{coef(summary(lm_in))},
 #' except with standard errors and the remaining statistics adjusted
 #' for heteroskedasticity.
 #'
+#' @seealso The \code{vcovHC} function in the \code{sandwich} package.
+#'
 white_hccme <- function(lm_in) {
 
 
-  # Standard erros are the square root of thediagonal of the
+  # Standard erros are the square root of the diagonal of the
   # White Heteroskedasticity-Corrected Covariance Matrix.
-  vcov_mat <- sandwich::vcovHC(lm_in)
+  # Note that type = 'HC' calculates White's estimator.
+  vcov_mat <- sandwich::vcovHC(lm_in, type = 'HC')
   White_se <- sqrt(diag(vcov_mat))
 
   # Recalculate the t-statistics and p-values.
@@ -27,13 +32,72 @@ white_hccme <- function(lm_in) {
   lm_stats_White[, 't value'] <- lm_stats_White[, 'Estimate'] / lm_stats_White[, 'Std. Error']
   # Recalculate the p-values.
   lm_stats_White[, 'Pr(>|t|)'] <- 2*pt(- abs(lm_stats_White[, 't value']),
-                                       df = lm_in$df.residuals)
+                                       df = lm_in$df.residual)
 
   lm_out <- list(vcov_hccme = vcov_mat,
                  coef_hccme = lm_stats_White)
 
   return(lm_out)
 }
+
+
+
+#' Calculate the Heteroskedasticity-Consistent Covariance Matrix Estimator slowly.
+#'
+#' \code{white_hccme_slow} is a helper function to calculate the HCCME,
+#' to test the calculation for un-aggregated data, i.e. data
+#' with weights either \code{NULL} or equal to 1.
+#' It calculates the covariance matrix from first principles, i.e. with loops.
+#'
+#' @param lm_in is an object of class "\code{lm}" that is the output of a
+#' linear regression model.
+#'
+#' @return A list containing two elements. The first is the HCCME covariance
+#' matrix. the second is a data frame of the form of \code{coef(summary(lm_in))},
+#' except with standard errors and the remaining statistics adjusted
+#' for heteroskedasticity.
+#'
+#' @seealso \code{white_hccme}, which uses \code{vcovHC} function in the
+#' \code{sandwich} package.
+#'
+white_hccme_slow <- function(lm_in) {
+
+  # Calculate the meat of the sandwich matrix.
+  if (length(ind_lpm_lm$x) == 0) {
+    stop('Missing the model matrix.',
+         'Set x = TRUE when calling the lm function.')
+  }
+  n <- nrow(lm_in$x)
+  k <- ncol(lm_in$x)
+  vcov_meat <- matrix(0, nrow = k, ncol = k)
+  for (i in 1:n) {
+    vcov_meat <- vcov_meat + lm_in$residuals[i]^2 *
+      t(lm_in$x[i, , drop = FALSE]) %*% lm_in$x[i, ]
+  }
+  vcov_bread <- solve(t(lm_in$x) %*% lm_in$x)
+  vcov_mat <- vcov_bread %*% vcov_meat %*% vcov_bread
+
+  # Standard erros are the square root of the diagonal of the
+  # White Heteroskedasticity-Corrected Covariance Matrix.
+  White_se <- sqrt(diag(vcov_mat))
+
+
+  # Recalculate the t-statistics and p-values.
+  lm_stats_White <- coef(summary(lm_in))
+  # Replace the SEs.
+  lm_stats_White[, 'Std. Error'] <- White_se
+  # Recalculate the t-stats.
+  lm_stats_White[, 't value'] <- lm_stats_White[, 'Estimate'] / lm_stats_White[, 'Std. Error']
+  # Recalculate the p-values.
+  lm_stats_White[, 'Pr(>|t|)'] <- 2*pt(- abs(lm_stats_White[, 't value']),
+                                       df = lm_in$df.residual)
+
+  lm_out <- list(vcov_hccme = vcov_mat,
+                 coef_hccme = lm_stats_White)
+
+  return(lm_out)
+}
+
 
 
 
--- dev/agg_het_comp.R
+++ dev/agg_het_comp.R
@@ -34,3 +34,78 @@
 # (Binary Dependent Variable)
 ##################################################
 
+#--------------------------------------------------
+# Generate sample data
+#--------------------------------------------------
+
+set.seed(42)
+
+ind_lpm_data <- gen_agg_lm(model = 'lpm')
+
+# Variables appear correct.
+# All inside the unit interval.
+summary(ind_lpm_data)
+
+
+# Aggregate the counts.
+ind_lpm_data[, 'num'] <- 1
+
+agg_lpm_data <- aggregate(num ~ y + x1 + x2 + x3,
+                          data = ind_lpm_data,
+                          FUN = sum)
+
+
+
+#--------------------------------------------------
+# Estimate Linear Model from Individual Data
+#--------------------------------------------------
+
+ind_lpm_lm <- lm(y ~ x1 + x2 + x3, data = ind_lpm_data,
+                 x = TRUE)
+
+# Correct the satandard errors for heteroskedasticity.
+ind_lpm_hccme <- white_hccme(ind_lpm_lm)
+
+# Compare the homoskedastic with the heteroskedastic cases.
+summary(ind_lpm_lm)
+print(ind_lpm_hccme$coef_hccme)
+# Looks plausible.
+# Compare it to reg y x, robust?
+
+# Recalculate from first principles.
+ind_lpm_hccme_slow <- white_hccme_slow(ind_lpm_lm)
+print(ind_lpm_hccme_slow$coef_hccme)
+# Exactly the same.
+# Check for accuracy of SEs alone:
+ind_lpm_hccme_slow$coef_hccme[, 'Std. Error'] /
+  ind_lpm_hccme$coef_hccme[, 'Std. Error']
+# Exactly the same.
+
+
+
+
+#--------------------------------------------------
+# Estimate Linear Model from Aggregated Data
+#--------------------------------------------------
+
+
+# Modified lm function.
+agg_lpm_lm <- agg_lm(y ~ x1 + x2 + x3, data = agg_lpm_data, weights = num)
+
+
+#--------------------------------------------------
+# Compare the results
+#--------------------------------------------------
+
+
+# Compare summaries.
+summary(ind_lpm_lm)
+summary_agg_lm(agg_lpm_lm)
+
+
+
+
+
+
+
+
